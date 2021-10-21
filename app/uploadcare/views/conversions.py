d8fbdb3e2cf775b7c2ca68f8e65e99b6cb00bccf
a.vasilyev <a.vasilyev@evrone.com>
2021-10-21 09:54:33 +0300

    document conversion

--- app/uploadcare/views/conversions.py
+++ app/uploadcare/views/conversions.py
@@ -2,9 +2,10 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.views.generic import FormView, TemplateView
 from pyuploadcare.dj.client import get_uploadcare_client
+from pyuploadcare.transformations.document import DocumentTransformation
 from pyuploadcare.transformations.video import VideoTransformation
 
-from uploadcare.forms import VideoConversionRequestForm
+from uploadcare.forms import DocumentConversionRequestForm, VideoConversionRequestForm
 
 
 class VideoConversionRequestView(FormView):
@@ -16,36 +17,26 @@ class VideoConversionRequestView(FormView):
 
         transformation = VideoTransformation()
 
-        target_format = data.get("format")
-        if target_format:
-            transformation = transformation.format(target_format)
+        if data["format"]:
+            transformation = transformation.format(data["format"])
 
-        quality = data.get("quality")
-        if quality:
-            transformation = transformation.quality(quality)
+        if data["quality"]:
+            transformation = transformation.quality(data["quality"])
 
-        resize_mode = data.get("resize_mode")
-        width = data.get("width")
-        height = data.get("height")
-        if width or height:
-            transformation = transformation.size(width, height, resize_mode)
+        if data["width"] or data["height"]:
+            transformation = transformation.size(data["width"], data["height"], data["resize_mode"])
 
-        start_time = data.get("start_time")
-        length = data.get("length")
-        if start_time and length:
-            transformation = transformation.cut(start_time, length)
+        if data["start_time"] and data["length"]:
+            transformation = transformation.cut(data["start_time"], data["length"])
 
-        thumbs = data.get("thumbs")
-        if thumbs:
-            transformation = transformation.thumbs(thumbs)
-
-        store = data.get("store")
+        if data["thumbs"]:
+            transformation = transformation.thumbs(data["thumbs"])
 
         uploadcare = get_uploadcare_client()
 
         file_id = data["file"]
         path = transformation.path(file_id)
-        response = uploadcare.video_convert_api.convert([path], store=store)
+        response = uploadcare.video_convert_api.convert([path], store=data["store"])
 
         if response.problems:
             for key, value in response.problems.items():
@@ -63,4 +54,44 @@ class VideoConversionJobStatusView(TemplateView):
         token = self.kwargs["token"]
         uploadcare = get_uploadcare_client()
         kwargs["job_status"] = uploadcare.video_convert_api.status(token)
+        return kwargs
+
+
+class DocumentConversionRequestView(FormView):
+    template_name = "conversions/document/request.html"
+    form_class = DocumentConversionRequestForm
+
+    def form_valid(self, form):  # noqa: C901
+        data = form.cleaned_data
+
+        transformation = DocumentTransformation()
+
+        if data["format"]:
+            transformation = transformation.format(data["format"])
+
+        if data["page"]:
+            transformation = transformation.page(data["page"])
+
+        uploadcare = get_uploadcare_client()
+
+        file_id = data["file"]
+        path = transformation.path(file_id)
+        response = uploadcare.document_convert_api.convert([path], store=data["store"])
+
+        if response.problems:
+            for key, value in response.problems.items():
+                messages.error(self.request, f"{key}: {value}")
+                return redirect("document_conversion_request")
+
+        token = response.result[0].token
+        return redirect("document_conversion_status", token)
+
+
+class DocumentConversionJobStatusView(TemplateView):
+    template_name = "conversions/document/status.html"
+
+    def get_context_data(self, **kwargs):
+        token = self.kwargs["token"]
+        uploadcare = get_uploadcare_client()
+        kwargs["job_status"] = uploadcare.document_convert_api.status(token)
         return kwargs
